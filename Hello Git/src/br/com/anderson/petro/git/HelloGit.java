bfc7c0a6942bd6148b9b09d7f7a15120e85ac249
Anderson Petró <66042820+AndersonPetro@users.noreply.github.com>
2020-11-12 13:17:28 -0300

    Adicionei arquivos via upload

---
+++ Hello Git/src/br/com/anderson/petro/git/HelloGit.java
@@ -0,0 +1,10 @@
+package br.com.anderson.petro.git;
+
+import jdk.dynalink.beans.StaticClass;
+
+public class HelloGit {
+    public static void main (String [] args){
+        System.out.println("hello Git");
+
+    }
+}
